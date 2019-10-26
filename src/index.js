d71fc8b30baf7d5e88414eaef891d9ce064c181e
Parker Cooper <parkr.coopr@gmail.com>
2019-10-26 16:13:54 -0400

    crud

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+  headers: {
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjazI3czk4bmV0dm53MDk5NHN2bzg3bjI1IiwiaWF0IjoxNTcyMTEwNjUwfQ.M4R_L4pf53E-V9YBWj3hf7nS3j1Ao9ph1Rchk17EVDM"
+  }
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root')
+)
 serviceWorker.unregister();
